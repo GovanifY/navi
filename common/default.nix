4f455b6e9052fe4c87eb1f4312ed3482f305b5c0
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2020-12-24 12:34:41 +0100

    command-not-found: generate nixexprs

--- common/default.nix
+++ common/default.nix
@@ -39,29 +39,18 @@
   };
   boot.cleanTmpDir = true;
 
-  # Add support for command-not-found. For simplicity, hardcode a Nix channel
-  # revision that has the programs.sqlite pregenerated instead of building it
-  # ourselves since that's expensive.
   environment.variables.NIX_AUTO_RUN = "1";
-  programs.command-not-found.dbPath = let
-    channelTarball = pkgs.fetchurl {
-      url = "https://releases.nixos.org/nixos/unstable/nixos-20.03pre193781.d484f2b7fc0/nixexprs.tar.xz";
-      sha256 = "0aqm56p66ys3nhy19j5zbj8agfg7dyccnxgplm5kdykv22p8h4gc";
-    };
-  in
-    pkgs.runCommand "programs.sqlite" {} ''
-      tar xf ${channelTarball} --wildcards "nixos*/programs.sqlite" -O > $out
-    '';
-    console.earlySetup = true;
-    boot.loader.timeout = 1;
+  programs.command-not-found.enable = true;
+  console.earlySetup = true;
+  boot.loader.timeout = 1;
 
-    # no UDP when through tor, so we use http date to synchronize the system
-    # clock
-    services.timesyncd.enable = false;
-    services.htpdate.enable = true;
-    services.htpdate.servers = [ "db.debian.org" "www.eff.org" "www.torproject.org" "cve.mitre.org"
-                                 "en.wikipedia.org" "google.com" "govanify.com" "lkml.org" "www.apache.org" 
-                                 "www.duckduckgo.com" "www.kernel.org" "www.mozilla.org" "www.xkcd.com"];
+  # no UDP when through tor, so we use http date to synchronize the system
+  # clock
+  services.timesyncd.enable = false;
+  services.htpdate.enable = true;
+  services.htpdate.servers = [ "db.debian.org" "www.eff.org" "www.torproject.org" "cve.mitre.org"
+                               "en.wikipedia.org" "google.com" "govanify.com" "lkml.org" "www.apache.org" 
+                               "www.duckduckgo.com" "www.kernel.org" "www.mozilla.org" "www.xkcd.com"];
 
 }
 
