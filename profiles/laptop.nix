bbc7e159efbfd9795399886d5c0fcac954fe816c
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2021-07-01 04:51:47 +0200

    profile/laptop: make autoupgrade state persistent and weekly

--- profiles/laptop.nix
+++ profiles/laptop.nix
@@ -10,5 +10,13 @@ with lib;
 
     navi.profile.graphical = true;
     navi.components.gaming.enable = true;
+
+    # check weekly for updates on laptop instead of the usual daily, a few hours
+    # after other devices updates so that they had the time to build the new
+    # updates and our laptop can fetch it from the binary cache. While we're
+    # at it use a persistent state so that systemd realize when the system was
+    # shut down for quite some time.
+    system.autoUpgrade.dates = "Mon *-*-* 20:00:00";
+    systemd.timers.nixos-upgrade.timerConfig.Persistent = "true";
   };
 }
