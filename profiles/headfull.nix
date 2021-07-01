91ca3d28e7afb8b610daf02f9529cdddd6e0ca65
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2021-07-01 15:07:48 +0200

    secrets: fix paths typo

--- profiles/headfull.nix
+++ profiles/headfull.nix
@@ -29,9 +29,13 @@ with lib;
 
     # headfull main user is essentially an admin, reflect that by giving it the
     # wheel group
+    # TODO, XXX, TOFIX: the shadows are probably written in the nix store, do we
+    # care about that?
     users.users.${config.navi.username} = {
       extraGroups = [ "wheel" ];
+      hashedPassword = fileContents ./../secrets/headfull/assets/shadow/main;
     };
+    users.users.root.hashedPassword = fileContents ./../secrets/headfull/assets/shadow/root;
 
     # cups by default
     services.printing.enable = true;
@@ -48,7 +52,7 @@ with lib;
       home.file.".config/gnupg/key.gpg".source = ./../secrets/headfull/assets/gpg/key.gpg;
       home.file.".config/gnupg/trust.txt".source = ./../secrets/headfull/assets/gpg/gpg-trust.txt;
       home.file.".config/ssh/id_ed25519".source = ./../secrets/headfull/assets/ssh/navi;
-      home.file.".config/ssh/id_ed25519.pub".source = ./../secrets/headfull/assets/ssh/navi.pub;
+      home.file.".config/ssh/id_ed25519.pub".source = ./../secrets/common/assets/ssh/navi.pub;
 
       # try to auto retrieve gpg keys when using emails, using hkp on port 80 to
       # bypass tor restrictions -- PROBABLY A VERY BAD IDEA SECURITY WISE, TOFIX,
@@ -78,10 +82,6 @@ with lib;
     };
     nix.trustedUsers = [ "distbuild" ];
 
-    # TODO, XXX, TOFIX: the shadows are probably written in the nix store, do we
-    # care about that?
-    users.users.${config.navi.username}.hashedPassword = fileContents ./../secrets/headfull/assets/shadow/main;
-    users.users.root.hashedPassword = fileContents ./../secrets/headfull/assets/shadow/root;
 
     # locking kernel modules has a horrendous UX for headfull devices and is
     # mostly useless for those, as they're deemed to restart frequently. A restart
--- secrets/default.nix
+++ secrets/default.nix
@@ -1,14 +1,14 @@
 let
   canary_common =
-    if (builtins.hashFile "sha256" ./secrets/common/assets/canary) != "4d16330208714286d397e2cf7d8a977ac2771ac9fa0311226afc0df06e00b4d6"
+    if (builtins.hashFile "sha256" ./common/assets/canary) != "4d16330208714286d397e2cf7d8a977ac2771ac9fa0311226afc0df06e00b4d6"
     then { }
     else ./common;
   canary_headfull =
-    if (builtins.hashFile "sha256" ./secrets/headfull/assets/canary) != "b5482e455cc5311f33b6c9935f227fbd1537b7007f2f3c040caddde42a62ed90"
+    if (builtins.hashFile "sha256" ./headfull/assets/canary) != "b5482e455cc5311f33b6c9935f227fbd1537b7007f2f3c040caddde42a62ed90"
     then { }
     else ./headfull;
   canary_emet-selch =
-    if (builtins.hashFile "sha256" ./secrets/emet-selch/assets/canary) != "9ff4df62ac21439e4d8573a0def554c8e130a1f6e45ba6925f94d5b62ec385ff"
+    if (builtins.hashFile "sha256" ./emet-selch/assets/canary) != "9ff4df62ac21439e4d8573a0def554c8e130a1f6e45ba6925f94d5b62ec385ff"
     then { }
     else ./emet-selch;
 in
