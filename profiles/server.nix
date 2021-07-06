c32b4176e00b45c15a329f2018339f54ad7b06bc
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2021-07-07 01:33:54 +0200

    profiles/server: make sure xdg is disabled

--- profiles/server.nix
+++ profiles/server.nix
@@ -2,7 +2,9 @@
 with lib;
 {
   config = mkIf (config.navi.profile.name == "server") {
-    services.postgresql.enable = true;
-    services.nginx.enable = true;
+    # the .ssh changes and others could lead to security issues on server for
+    # apps interacting with it, moreso we do not care about the ux on servers
+    # and this makes it so our server don't have to compile anything, so rip xdg
+    navi.components.xdg.enable = mkForce false;
   };
 }
