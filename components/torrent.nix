200ec39877a43ee2a4f5d1b2464d60b82ebdd6c6
Gauvain 'GovanifY' Roussel-Tarbouriech <gauvain@govanify.com>
2022-07-16 04:59:03 +0200

    Revert "components/torrent: change back to default file handles"
    This reverts commit fce0f38100c8248886de5ae0e04c79c853688641.
    turns out rtorrent is still as broken

--- components/torrent.nix
+++ components/torrent.nix
@@ -47,6 +47,13 @@ in
 
         protocol.encryption.set = allow_incoming,try_outgoing,enable_retry
 
+        # Limits for file handle resources, this is optimized for
+        # an `ulimit` of 1024 (a common default). You MUST leave
+        # a ceiling of handles reserved for rTorrent's internal needs!
+        network.http.max_open.set = 50
+        network.max_open_files.set = 600
+        network.max_open_sockets.set = 300
+
         pieces.memory.max.set = 4000M
         network.xmlrpc.size_limit.set = 4M
 
