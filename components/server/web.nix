ca242bfef8e626cf7b85d669e719c730429ebc56
GovanifY <me@govanify.com>
2023-04-16 19:31:28 +0200

    components/web: give more resources to phpfpm

--- components/server/web.nix
+++ components/server/web.nix
@@ -160,12 +160,12 @@ in
       user = "nobody";
       group = "nogroup";
       settings = {
-        pm = "dynamic";
+        "pm" = "dynamic";
         "listen.owner" = config.services.nginx.user;
-        "pm.max_children" = 5;
-        "pm.start_servers" = 2;
-        "pm.min_spare_servers" = 1;
-        "pm.max_spare_servers" = 3;
+        "pm.max_children" = 100;
+        "pm.start_servers" = 15;
+        "pm.min_spare_servers" = 10;
+        "pm.max_spare_servers" = 30;
         "pm.max_requests" = 500;
       };
     };
