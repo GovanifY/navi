8a813abc810d64a985fe23955a171e5cc1d51d72
GovanifY <me@govanify.com>
2024-03-19 18:52:39 +0100

    infrastructure/alastor: enable vaapi hw encoding for intel default libva to amd, enable opencl this fixes some issues with amaurot

--- infrastructure/alastor/hardware.nix
+++ infrastructure/alastor/hardware.nix
@@ -96,9 +96,21 @@ in
       openrazer-daemon
       polychromatic
     ];
-    hardware.opengl.extraPackages = with pkgs; [
-      rocmPackages.clr.icd
-    ];
+
+    nixpkgs.config.packageOverrides = pkgs: {
+      vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+    };
+    hardware.opengl = {
+      enable = true;
+      extraPackages = with pkgs; [
+        vaapiIntel
+        vaapiVdpau
+        libvdpau-va-gl
+        intel-media-driver
+        rocmPackages.clr.icd
+      ];
+    };
+    environment.sessionVariables = { LIBVA_DRIVER_NAME = "radeonsi"; };
 
     services.btrfs.autoScrub.fileSystems = axolotl_fs ++ [ "/" ];
 
