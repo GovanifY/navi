441bf7ce8978ef9d290fae0738728b7281577171
GovanifY <me@govanify.com>
2024-04-09 13:29:43 +0200

    secrets/amaurot: update

--- components/headfull/chat.nix
+++ components/headfull/chat.nix
@@ -15,8 +15,10 @@ let
           weechat-autosort
           weechat-matrix
           weechat-go
+          weechat-otr
           highmon
           buffer_autoset
+          colorize_nicks
         ];
       };
   };
@@ -38,5 +40,6 @@ in
     ] ++ optionals cfg.graphical [
       element-desktop-wayland
     ];
+    services.weechat.binary = "${weechat}/bin/weechat";
   };
 }
