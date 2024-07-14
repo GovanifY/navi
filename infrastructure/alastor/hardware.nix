5382ad2881b85869796d875cb7c54bffd85a316a
GovanifY <me@govanify.com>
2024-07-14 21:13:02 +0200

    infrastructure/alastor: increase maximum tmp usage

--- infrastructure/alastor/hardware.nix
+++ infrastructure/alastor/hardware.nix
@@ -126,6 +126,7 @@ in
     environment.sessionVariables = { LIBVA_DRIVER_NAME = "radeonsi"; };
 
     services.btrfs.autoScrub.fileSystems = axolotl_fs ++ [ "/" "/mnt/violet" ];
+    services.logind.extraConfig = "RuntimeDirectorySize=20G";
 
     # that one's a doozy, so to explain: For each fs in our scrub list, we
     # define after to another substituted list in the let at the header which
