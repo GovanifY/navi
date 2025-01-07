c9c92ac7ebde944ddcbe95a4e646dc756e7c4519
GovanifY <me@govanify.com>
2025-01-07 15:49:44 +0100

    profiles/graphical: enable usb block on lockscreen

--- profiles/graphical.nix
+++ profiles/graphical.nix
@@ -67,5 +67,26 @@ with lib;
     programs.kdeconnect.enable = true;
     programs.partition-manager.enable = true;
 
+
+    # usbguard and polkit rules so that you cannot plug new usb when on login
+    # screen on gnome
+    security.polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        if ((action.id == "org.usbguard.Policy1.listRules" ||
+             action.id == "org.usbguard.Policy1.appendRule" ||
+             action.id == "org.usbguard.Policy1.removeRule" ||
+             action.id == "org.usbguard.Devices1.applyDevicePolicy" ||
+             action.id == "org.usbguard.Devices1.listDevices" ||
+             action.id == "org.usbguard1.getParameter" ||
+             action.id == "org.usbguard1.setParameter") &&
+             subject.active == true && subject.local == true &&
+             subject.isInGroup("wheel")) { return polkit.Result.YES; }
+      });
+    '';
+    services.usbguard = {
+      enable = true;
+      dbus.enable = true;
+      implicitPolicyTarget = "allow";
+    };
   };
 }
