6abd7919947a204e7a72b9077b6615fed3d5c8ce
GovanifY <me@govanify.com>
2026-01-23 20:59:42 +0100

    components/torrent: hotfix

--- components/torrent.nix
+++ components/torrent.nix
@@ -30,6 +30,11 @@ in
         schedule = disableudp, 0, 1, trackers.use_udp.set=no
         trackers.use_udp.set = no
 
+        network.max_open_files.set = 600
+        network.max_open_sockets.set = 300
+        network.http.max_open.set = 32
+
+
         method.redirect=load.throw,load.normal
         method.redirect=load.start_throw,load.start
         method.insert=d.down.sequential,value|const,0
@@ -39,6 +44,39 @@ in
 
     # chown segfault...
     systemd.services.rtorrent.serviceConfig.SystemCallFilter = lib.mkForce [ ];
+
+    # HOTFIX: 0.16.5 OOMs and 0.16.6 crashes at startup because of a socket issue, let's
+    # revert to 0.16.4 for now...
+    nixpkgs.overlays = [
+      (
+        self: super: {
+          libtorrent-rakshasa = super.libtorrent-rakshasa.overrideAttrs (old: {
+            version = "0.16.4";
+
+            src = self.fetchFromGitHub {
+              owner = "rakshasa";
+              repo = "libtorrent";
+              tag = "v0.16.4";
+              hash = "sha256-r+5rNaBXhHbDWFXbgEPriEmjWEjTyu2I5H7rl3PoF38=";
+            };
+          });
+
+          rtorrent = super.rtorrent.overrideAttrs (old: {
+            version = "0.16.4";
+
+            src = self.fetchFromGitHub {
+              owner = "rakshasa";
+              repo = "rtorrent";
+              tag = "v0.16.4";
+              hash = "sha256-ut1R73UfkpDk/Y5Fq8kSavxIB3Y2jbYEQ8J/559Ech0=";
+            };
+          });
+
+        }
+      )
+    ];
+
+
 
     systemd.services."flood" = {
       enable = true;
